538cab3d9c0cf113e50da55eb7a2ab93a02565c2
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2011-03-27 12:06:04 +0200

    core: Use GOption instead of getopt
    getopt is not available everywhere.

--- src/snappy.c
+++ src/snappy.c
@@ -25,24 +25,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <getopt.h>
 #include <clutter/clutter.h>
 #include <clutter-gst/clutter-gst.h>
 
 #include "user_interface.h"
 #include "gst_engine.h"
 #include "utils.h"
-
-
-static void
-help (const char *argv0)
-{
-  g_print (("%s [options] media_file\n\n"
-          " -h, --help                           Show this help\n"
-          "     --version                        Show this version\n\n"
-          " -f, --fullscreen                     Play video fullscreen\n\n")
-      , argv0);
-}
 
 int
 main (int argc, char *argv[])
@@ -52,37 +40,45 @@ main (int argc, char *argv[])
   ClutterActor *texture;
   gchar *fileuri;
   int ret = 0;
-  gboolean fullscreen = FALSE;
+  gboolean fullscreen = FALSE, version = FALSE;
   guint c, index, pos = 0;
   gchar *file_list[argc];
-
-  static const struct option long_options[] = {
-    {"fullscreen", 0, NULL, 'f'},
-    {"help", 0, NULL, 'h'},
-    {"version", 0, NULL, 'v'}
+  GOptionEntry entries[] = {
+    {"fullscreen", 'f', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_NONE, &fullscreen,
+        "Fullscreen mode", NULL},
+    {"version", 'v', G_OPTION_FLAG_IN_MAIN, G_OPTION_ARG_NONE, &version,
+        "Print version", NULL},
+    {NULL}
   };
+  GOptionContext *context;
+  GError *err = NULL;
+
+  if (!g_thread_supported ())
+    g_thread_init (NULL);
+
+  context = g_option_context_new ("<media file> - Play movie files");
+  g_option_context_add_main_entries (context, entries, NULL);
+  g_option_context_add_group (context, gst_init_get_option_group ());
+  g_option_context_add_group (context, clutter_get_option_group ());
 
   // Command line arguments.
-  if (argc < 2) {
-    g_print ("Usage: %s [options] <media_file>\n", argv[0]);
+  if (!g_option_context_parse (context, &argc, &argv, &err)) {
+    g_print ("Error initializing: %s\n", err->message);
+    g_error_free (err);
     goto quit;
   }
 
-  while ((c = getopt_long (argc, argv, "fh", long_options, NULL)) != -1)
-    switch (c) {
-      case 'f':
-        g_debug ("fullscreen!\n");
-        fullscreen = TRUE;
-        break;
-      case 'h':
-        help (argv[0]);
-        goto quit;
-      case 'v':
-        g_print ("snappy version %s\n", VERSION);
-        goto quit;
-    }
+  if (version) {
+    g_print ("snappy version %s\n", VERSION);
+    goto quit;
+  }
+  // File arguments
+  if (argc < 2) {
+    g_print ("%s", g_option_context_get_help (context, TRUE, NULL));
+    goto quit;
+  }
 
-  for (index = optind; index < argc; index++) {
+  for (index = 1; index < argc; index++) {
     file_list[pos] = argv[index];
     g_debug ("Adding file: %s\n", file_list[pos]);
     pos++;
@@ -134,5 +130,7 @@ main (int argc, char *argv[])
   clutter_main ();
 
 quit:
+  g_option_context_free (context);
+
   return ret;
 }
